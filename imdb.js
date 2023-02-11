0ea412d483426521968222311e90a5326d521034
Sabri Gülseven <sabrigulseven@outlook.com>
2023-02-11 22:48:59 +0300

    v1.1.1

--- imdb.js
+++ imdb.js
@@ -6,7 +6,11 @@ let rarbgLink = "https://rarbgenter.org/torrents.php?search=" + imdbId;
 const button = buttonRarbg(rarbgLink);
 
 var div = openDiv();
-var p = document.getElementsByClassName('sc-11c0820-3 hwmGqL')[1]; // classname of all block
+//var p = document.getElementsByClassName('sc-11c0820-3 hwmGqL')[1]; // classname of all block
+//var p = getElementByXpath('//*[@id="__next"]/main/div/section[1]/section/div[3]/section/section/div[1]/div/div[2]')
+var p = document.querySelector('[data-testid="hero-subnav-bar-right-block"]')
+
+
 
 div.appendChild(button);
 p.appendChild(div);
@@ -33,3 +37,7 @@ function openDiv() {
     //div.innerHTML = ' <div class="sc-f172496b-1 kXVeCj">  Text  </div>';
     return div;
 }
+
+function getElementByXpath(path) {
+    return document.evaluate(path, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+  }
